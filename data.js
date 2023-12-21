8d321dfcd666db7ed5b7c01a19be2fa45232c4b7
fellowyellow17125 <133915690+fellowyellow17125@users.noreply.github.com>
2023-12-21 03:41:12 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+const chartData = [
+    ['Arts/Journalism/Media', 12],
+    ['Consulting', 3],
+    ['Corporate & All Other Business', 4],
+    ['Education', 6],
+    ['Entrepreneurship / Start-Ups', 9],
+    ['Financial Services', 12],
+    ['Government / Policy/Non-Profits', 21],
+    ['Healthcare', 6],
+    ['Law', 3],
+    ['Science & Technology', 22]
+  ];
+  
